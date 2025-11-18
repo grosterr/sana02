714b73f9dd9b89b9aa434807c6dc6f3ab33a9de3
Dima <ipz252_gdo@student.ztu.edu.ua>
2025-11-18 17:14:34 +0200

    completed

--- While/While.cs
+++ While/While.cs
@@ -1,2 +1,30 @@
 ﻿using System.Text;
+using System;
 Console.OutputEncoding = UTF8Encoding.UTF8;
+int n, i = 0, fact = 0;
+double b, c = 1, d = 0;
+Console.WriteLine("Введіть n: "); n = int.Parse(Console.ReadLine());
+while (i <= n)
+        {
+                b = Math.Pow(-1, i + 1);
+                c += 2;
+                d += b / c;
+                i++;
+            }
+            Console.WriteLine($"a = {d}");
+i = 1;
+b = 0;
+while(i <= n)
+{
+    b += 1 + (1 / Math.Pow(i, 2));
+    i++;
+}
+Console.WriteLine($"b = {b}");
+i = 1;
+b = 0; c = 1;
+while (i <= n)
+{
+    c *= i;
+    i++;
+}
+Console.WriteLine($"c = {c}");
